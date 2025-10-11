648b4347780381a50ca9b35a7123f6fc8a688587
Daniel Canlapan <danielcanlapan0402@gmail.com>
2025-10-11 12:02:52 -0700

    Update AllInHomepage.py
    add colorschemes, font, icons

--- All-In/AllInHomepage.py
+++ All-In/AllInHomepage.py
@@ -1,120 +1,150 @@
 import customtkinter as ctk
 from tkinter import font, filedialog  
 import os  
-from PIL import ImageFont
-
+from PIL import Image, ImageFont
+import matplotlib.font_manager as fm
 
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
+# Color theme
+ctk.set_default_color_theme("Harlequin.json")
 
 app = ctk.CTk()
 app.geometry("1080x720")
 app.title("AllIn")
 
-font_dir = os.path.join(os.path.dirname(__file__), "fonts")
-#font_dir = r"C:\Users\danie\OneDrive\Desktop\Documents\AllIn\static" Folder where font is saved
-montserrat_regular_path = os.path.join(font_dir, "Montserrat-Regular.ttf")
-montserrat_bold_path = os.path.join(font_dir, "Montserrat-Bold.ttf")
+font_dir = os.path.join(os.path.dirname(__file__), "MontserratFont")
+montserrat_semibold_path = os.path.join(font_dir, "Montserrat-SemiBold.ttf")
+montserrat_black_path = os.path.join(font_dir, "Montserrat-Black.ttf")
 
 #Fonts
 try:
-    if os.path.exists(montserrat_regular_path):
-        app.call("font", "create", "Montserrat", "-family", "Montserrat", "-file", montserrat_regular_path)
-    else:
-        print(f"Warning: {montserrat_regular_path} not found. Using default font.")
-        montserrat_regular = ("Arial", 16) 
-        montserrat_bold = ("Arial", 36, "bold")  
+    if not os.path.exists(montserrat_semibold_path):
+        print(f"Warning: {montserrat_semibold_path} not found.")
+        montserrat_semibold = ("Arial", 16) 
+        montserrat_black = ("Arial", 36, "bold")  
         raise FileNotFoundError
 
-    if os.path.exists(montserrat_bold_path):
-        app.call("font", "create", "MontserratBold", "-family", "Montserrat Bold", "-file", montserrat_bold_path)
-    else:
-        print(f"Warning: {montserrat_bold_path} not found. Using default font.")
-        montserrat_regular = ("Arial", 16)  
-        montserrat_bold = ("Arial", 36, "bold") 
+    if not os.path.exists(montserrat_black_path):
+        print(f"Warning: {montserrat_black_path} not found.")
+        montserrat_semibold = ("Arial", 16)  
+        montserrat_black = ("Arial", 36, "bold") 
         raise FileNotFoundError
 
+    fm.fontManager.addfont(montserrat_semibold_path)
+    fm.fontManager.addfont(montserrat_black_path)
 
-    montserrat_regular = ("Montserrat", 16)
-    montserrat_bold = ("Montserrat Bold", 36)
+    font_properties_semibold = fm.FontProperties(fname=montserrat_semibold_path)
+    font_properties_black = fm.FontProperties(fname=montserrat_black_path)
+    montserrat_semibold_name = font_properties_semibold.get_name()
+    montserrat_black_name = font_properties_black.get_name()
+
+    montserrat_semibold = (montserrat_semibold_name, 16, "normal")
+    montserrat_black = (montserrat_black_name, 36, "bold")
+
+    if montserrat_semibold_name not in font.families():
+        print(f"Warning: {montserrat_semibold_name} not found.")
+        montserrat_semibold = ("Arial", 16)
+    if montserrat_black_name not in font.families():
+        print(f"Warning: {montserrat_black_name} not found.")
+        montserrat_black = ("Arial", 36, "bold")
+
 except FileNotFoundError:
-    montserrat_regular = ("Arial", 16)
-    montserrat_bold = ("Arial", 36, "bold")
+    print("Font files not found.")
+    montserrat_semibold = ("Arial", 16)
+    montserrat_black = ("Arial", 36, "bold")
+except Exception as e:
+    print(f"Font loading error: {e}")
+    montserrat_semibold = ("Arial", 16)
+    montserrat_black = ("Arial", 36, "bold")
 
 def clear_window():
     for widget in app.winfo_children():
         widget.destroy()
 
 def show_homepage():
-
     clear_window()
 
-    #homepage frame
-    frame = ctk.CTkFrame(master=app, fg_color="#521515")
+    # homepage frame 
+    frame = ctk.CTkFrame(master=app)
     frame.pack(pady=10, padx=10, fill="both", expand=True)
 
-    #Logo
+    # Logo
     logo_label = ctk.CTkLabel(
         master=frame,
         text="AllIN",
-        font=montserrat_bold,  
-        text_color="white"
+        font=montserrat_black,  
     )
     logo_label.place(relx=0.5, rely=0.25, anchor="center")
 
-    # Button frame
-    button_frame = ctk.CTkFrame(master=frame, fg_color="#521515")
+    # Button frame 
+    button_frame = ctk.CTkFrame(master=frame)
     button_frame.pack(expand=True, pady=100)
+
+    # start icon
+    start_icon_path = os.path.join(os.path.dirname(__file__), "png_icons", "start.png")
+    start_image = ctk.CTkImage(light_image=Image.open(start_icon_path), dark_image=Image.open(start_icon_path), size=(24, 24)) if os.path.exists(start_icon_path) else None
 
     # Start Button
     start_button = ctk.CTkButton(
         master=button_frame, 
-        text="Start",
-        fg_color=("#1E1E1E"),
-        text_color="white",
+        text="Start Learning",
         width=300,  
         height=50,
-        font=montserrat_regular,  
-        command=show_start_page
+        font=montserrat_semibold,  
+        command=show_start_page,
+        image=start_image,
+        compound="left"
     )
     start_button.pack(pady=10, padx=10)
 
-    # Create Flashcards Button
+    # Create icon
+    create_icon = os.path.join(os.path.dirname(__file__), "png_icons", "create.png")
+    create_image = ctk.CTkImage(light_image=Image.open(create_icon), dark_image=Image.open(create_icon), size=(24, 24)) if os.path.exists(start_icon_path) else None
+
+    # Create Flashcards Button 
     create_button = ctk.CTkButton(
         master=button_frame, 
         text="Create Flashcards",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=300,
         height=50,
-        font=montserrat_regular,  
-        command=show_create_flashcards_page
+        font=montserrat_semibold,  
+        command=show_create_flashcards_page,
+        image=create_image,
+        compound="left"
     )
     create_button.pack(pady=10, padx=10)
 
-    # Upload PDF Button
+    # Upload icon
+    upload_icon = os.path.join(os.path.dirname(__file__), "png_icons", "upload.png")
+    uplaod_image = ctk.CTkImage(light_image=Image.open(upload_icon), dark_image=Image.open(upload_icon), size=(24, 24)) if os.path.exists(start_icon_path) else None
+
+    # Upload PDF Button 
     upload_button = ctk.CTkButton(
         master=button_frame, 
         text="Upload PDF",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=300,
         height=50,
-        font=montserrat_regular,  
-        command=show_upload_pdf_page
+        font=montserrat_semibold,  
+        command=show_upload_pdf_page,
+        image=uplaod_image,
+        compound="left"
     )
     upload_button.pack(pady=10, padx=10)
 
-    # Exit Button
+    # Exit icon
+    exit_icon = os.path.join(os.path.dirname(__file__), "png_icons", "exit.png")
+    exit_image = ctk.CTkImage(light_image=Image.open(exit_icon), dark_image=Image.open(exit_icon), size=(24, 24)) if os.path.exists(start_icon_path) else None
+
+    # Exit Button 
     exit_button = ctk.CTkButton(
         master=button_frame, 
         text="Exit",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=300,
         height=50,
-        font=montserrat_regular,  
-        command=app.quit
+        font=montserrat_semibold,  
+        command=app.quit,
+        image=exit_image,
+        compound="left"
     )
     exit_button.pack(pady=10, padx=10)
 
@@ -122,14 +152,13 @@ def show_start_page():
     """Display the Start page."""
     clear_window()
 
-    frame = ctk.CTkFrame(master=app, fg_color="#521515")
+    frame = ctk.CTkFrame(master=app)
     frame.pack(pady=10, padx=10, fill="both", expand=True)
 
     title_label = ctk.CTkLabel(
         master=frame,
         text="Welcome to Start Page!",
-        font=montserrat_bold, 
-        text_color="white"
+        font=montserrat_black, 
     )
     title_label.pack(pady=50)
 
@@ -137,11 +166,9 @@ def show_start_page():
     back_button = ctk.CTkButton(
         master=frame,
         text="Back to Home",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=200,
         height=40,
-        font=montserrat_regular,
+        font=montserrat_semibold,
         command=show_homepage
     )
     back_button.pack(pady=20)
@@ -150,73 +177,64 @@ def show_create_flashcards_page():
     """Display the Create Flashcards page."""
     clear_window()
 
-    flashcard_frame = ctk.CTkFrame(master=app, fg_color="#521515")
-    flashcard_frame.pack(pady=10, padx=10, fill="both", expand=True)
+    frame = ctk.CTkFrame(master=app)
+    frame.pack(pady=10, padx=10, fill="both", expand=True)
 
-    # Title
+    # Title 
     title_label = ctk.CTkLabel(
-        master=flashcard_frame,
+        master=frame,
         text="Create Flashcards",
-        font=montserrat_bold,  
-        text_color="white"
+        font=montserrat_black,  
     )
     title_label.pack(pady=20)
 
     # Question input
     question_label = ctk.CTkLabel(
-        master=flashcard_frame,
+        master=frame,
         text="Question:",
-        font=montserrat_regular,  
-        text_color="white"
+        font=montserrat_semibold,  
     )
     question_label.pack(pady=5)
     question_entry = ctk.CTkEntry(
-        master=flashcard_frame,
+        master=frame,
         width=400,
         height=30,
         placeholder_text="Enter the question",
-        font=montserrat_regular 
+        font=montserrat_semibold 
     )
     question_entry.pack(pady=5)
 
     # Answer input
     answer_label = ctk.CTkLabel(
-        master=flashcard_frame,
+        master=frame,
         text="Answer:",
-        font=montserrat_regular,  
-        text_color="white"
+        font=montserrat_semibold,  
     )
     answer_label.pack(pady=5)
     answer_entry = ctk.CTkEntry(
-        master=flashcard_frame,
+        master=frame,
         width=400,
         height=30,
         placeholder_text="Enter the answer",
-        font=montserrat_regular  
+        font=montserrat_semibold  
     )
     answer_entry.pack(pady=5)
 
-    #Save Button (placeholder)
     save_button = ctk.CTkButton(
-        master=flashcard_frame,
+        master=frame,
         text="Save Flashcard",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=200,
         height=40,
-        font=montserrat_regular  
+        font=montserrat_semibold  
     )
     save_button.pack(pady=20)
 
-    #Back Button
     back_button = ctk.CTkButton(
-        master=flashcard_frame,
+        master=frame,
         text="Back to Home",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=200,
         height=40,
-        font=montserrat_regular,  
+        font=montserrat_semibold,  
         command=show_homepage
     )
     back_button.pack(pady=10)
@@ -225,15 +243,14 @@ def show_upload_pdf_page():
     """Display the Upload PDF page."""
     clear_window()
 
-    upload_frame = ctk.CTkFrame(master=app, fg_color="#521515")
+    upload_frame = ctk.CTkFrame(master=app)
     upload_frame.pack(pady=10, padx=10, fill="both", expand=True)
 
-    #App Title
+    #App Title 
     title_label = ctk.CTkLabel(
         master=upload_frame,
         text="Upload PDF",
-        font=montserrat_bold,  
-        text_color="white"
+        font=montserrat_black,  
     )
     title_label.pack(pady=20)
 
@@ -241,8 +258,7 @@ def show_upload_pdf_page():
     selected_file_label = ctk.CTkLabel(
         master=upload_frame,
         text="No file selected",
-        font=montserrat_regular,  
-        text_color="white"
+        font=montserrat_semibold,  
     )
     selected_file_label.pack(pady=10)
 
@@ -250,28 +266,22 @@ def show_upload_pdf_page():
     upload_button = ctk.CTkButton(
         master=upload_frame,
         text="Select PDF File",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=200,
         height=40,
-        font=montserrat_regular  
+        font=montserrat_semibold  
     )
     upload_button.pack(pady=20)
 
-    # Back Button
+    # Back Button 
     back_button = ctk.CTkButton(
         master=upload_frame,
         text="Back to Home",
-        fg_color=("#1E1E1E"),
-        text_color="white",
         width=200,
         height=40,
-        font=montserrat_regular,  
+        font=montserrat_semibold,  
         command=show_homepage
     )
     back_button.pack(pady=10)
 
-
 show_homepage()
-
 app.mainloop()
